f2ad51b06966371e31d197b6e237a033abc307a2
AOA19 <aakinyemi19@gmail.com>
2023-09-23 23:31:25 -0400

    Switched from parcel build tool to vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+export default {
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+  // Temporarily hide sass deprecation warning
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+      },
+    },
+  },
+};
